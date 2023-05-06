7827b4a3ad9a2d2fd05c2e5b5e943b0272912ab1
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2023-05-05 22:16:32 -0300

    update Courses data

--- src/pages/About/sections/Courses/data.js
+++ src/pages/About/sections/Courses/data.js
@@ -6,7 +6,7 @@ export const data = [
 		url: "https://suicobrasileira.sp.senai.br/curso/85566/115/tecnico-em-desenvolvimento-de-sistemas",
 		year: "2021-2022",
 		description:
-			"Lorem ipsum dolor sit amet consectetur adipiscing elit curabitur tellus, ridiculus sociosqu habitasse mauris pulvinar curae himenaeos suspendisse congue quam, libero vitae sodales eu cras nascetur antea. Magnis inceptos consequat est dignissim nec vulputate nisl facilisi interdum faucibus hendrerit purus viverra mollis turpis urna nibh vivamus id sapien commodo.",
+			"Este curso foi meu primeiro contato com programação e me proporcionou excelentes professores e ensinamentos. Adquiri base em diversas áreas da tecnologia, como Sistemas Operacionais, Sistemas Embarcados, Servidores, Hardwares e Redes. Aprendi lógica de programação, desenvolvimento Full Stack, Banco de Dados, Git e GitHub, Cloud, Docker, Linux, e Metodologias de Gerenciamento de projetos.",
 	},
 	{
 		id: 2,
